bf3d3d05c6f68e5161b9b8e8c23e56d5a6a9f0e9
wjstpgh <sehoJ3756@gmail.com>
2022-05-02 17:08:22 +0900

    mouse event

---
+++ element/js/이미지설명이벤트.js
@@ -0,0 +1,8 @@
+function showDetail(){//상세설명보게하고 버튼숨기기
+    document.querySelector('#desc').style.display='block';
+    document.querySelector('#open').style.display='none';
+}
+function hideDetail(){//상세설명숨기고 버튼숨기기
+    document.querySelector('#desc').style.display='none';
+    document.querySelector('#open').style.display='block';
+}
